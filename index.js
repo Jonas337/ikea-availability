2918f23129e6c1b956d2950b3f45bebb93f28d57
jonas <jonas.eichinger.biz@gmail.com>
2020-05-27 14:59:55 -0400

    added store param to config, changed output message
    added a store parameter to the configuration file, added it to the example json file, and added the checkout link to the sms sent, incase you already have a checkout cart saved on your phone, and want to continue without switching to a computer.
    
    also changed the debug message to specify click and collect instead of store open status.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const config = require('./config.json')
 const axios = require('axios')
 const sendMessage = require('./sms')
 const cron = require('node-cron')
+const storeName = config.storeName;
 
 let task = cron.schedule('* * * * *', () => {
-    console.log('Is IKEA open yet?')
+    console.log('Is IKEA click & collect available at your store yet?')
     let storeAvailability = axios.get('https://ikea-status.dong.st/latest.json').then((response) => {
         let data = response.data
         let dataArray = JSON.stringify(data)
 
         let result = data.filter(entry =>
-            entry.name === 'Baltimore' && entry.last_closed < entry.last_open
+            entry.name === storeName && entry.last_closed < entry.last_open
         )
 
         if (result.length === 1) {
--- sms.js
+++ sms.js
@@ -2,11 +2,12 @@ const config = require('./config.json')
 const accountSid = config.accountSid;
 const authToken = config.authToken;
 const client = require('twilio')(accountSid, authToken);
+const storeName = config.storeName;
 
 sendMessage = () => {
     client.messages
         .create({
-            body: 'IKEA East Palo Alto store available for Click & Collect',
+            body: 'IKEA ' + storeName +' store available for Click & Collect https://order.ikea.com/us/en/checkout/delivery/',
             from: config.fromPhoneNumber,
             to: config.toPhoneNumber
         })
